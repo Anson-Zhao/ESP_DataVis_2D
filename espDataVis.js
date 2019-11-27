38f0a4baf8a280b487c993c6bc22fb6d4a9f89d7
imacbig10 <lin.feng@g.northernacademy.org>
2019-11-27 10:07:11 -0500

    new Gauges

--- espDataVis.js
+++ espDataVis.js
@@ -46,13 +46,17 @@ function avg(res,len){
 
 var pastTime = "4m";
 var nowTime = "2m";
-var queryA = 'SELECT MEAN("X"), MEAN("Y"), MEAN("Z") FROM station_one WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime + ' GROUP BY time(1s)';
-var queryA2 = 'SELECT MEAN("X"), MEAN("Y"), MEAN("Z") FROM station_two WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime + ' GROUP BY time(1s)';
+var queryA = 'SELECT * FROM station_oneavg WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime;
+var queryA2 = 'SELECT * FROM station_twoavg WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime;
+var queryA3 = 'SELECT * FROM station_theavg WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime;
+// var backup = 'SELECT MEAN("X"), MEAN("Y"), MEAN("Z") FROM station_twoavg WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime + ' GROUP BY time(1s)';
 // var queryX = "SELECT MEAN(\"X\") FROM ar WHERE time>'2019-07-23T20:25:00Z' AND time<'2019-07-23T20:29:00Z' GROUP BY time(1s)";
 // var queryY = "SELECT MEAN(\"Y\") FROM ar WHERE time>'2019-07-23T20:25:00Z' AND time<'2019-07-23T20:29:00Z' GROUP BY time(1s)";
 // var queryZ = "SELECT MEAN(\"Z\") FROM ar WHERE time>'2019-07-23T20:25:00Z' AND time<'2019-07-23T20:29:00Z' GROUP BY time(1s)";
-var x_csv,y_csv,z_csv,gx,gy,gz;
-var life = [];
+// var x_csv,y_csv,z_csv,gx,gy,gz;
+// var life = [];
+// var ex='SELECT MEAN("X"), MEAN("Y"), MEAN("Z") FROM ' + req.query.stationIs + ' WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'" + ' GROUP BY time(1s)';
+// var ex2='SELECT "X", "Y", "Z" FROM ' + req.query.stationIs + ' WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'" + ' GROUP BY time(1s)';
 
 app.get('/query', function (req, res) {
 
@@ -76,9 +80,30 @@ app.get('/query2', function (req, res) {
         console.log(err)
     });
 });
-app.get('/newWind', function (req, res) {
-    var queryH = 'SELECT MEAN("X"), MEAN("Y"), MEAN("Z") FROM ' + req.query.stationIs + ' WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'" + ' GROUP BY time(1s)';
 
+app.get('/query3', function (req, res) {
+    influx.query(queryA3).then
+    (result => {
+        console.log(result);
+        res.send(result);
+    }).catch(err => {
+        console.log(err)
+    });
+});
+app.get('/newWind', function (req, res) {
+    var queryHa = 'SELECT * FROM ' + req.query.stationIs + 'avg WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'";
+// console.log(queryHa);
+    influx.query(queryHa).then
+    (result => {
+        res.send(result);
+        console.log(result)
+    }).catch(err => {
+        res.status(500).send(err.stack)
+    });
+});
+app.get('/newSnow', function (req, res) {
+    var queryH = 'SELECT * FROM ' + req.query.stationIs + ' WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'";
+// console.log(queryH);
     influx.query(queryH).then
     (result => {
         res.send(result);
@@ -87,4 +112,4 @@ app.get('/newWind', function (req, res) {
         res.status(500).send(err.stack)
     });
 });
-app.listen('3006');
+app.listen('3005');
