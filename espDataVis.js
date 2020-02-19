ea32eafbedc9d8bd47ddd42dc5bdb933447b2650
Anson-Zhao <ansonzmj@gmail.com>
2020-02-19 11:26:10 -0500

    V1.43

--- espDataVis.js
+++ espDataVis.js
@@ -1,11 +1,12 @@
 const Influx = require('influx');
 const express = require('express');
 const app = express();
-var jsonexport = require('jsonexport');
-var path = require('path');
-const csv = require('csv-parser');
-var fs = require('fs');
+// let jsonexport = require('jsonexport');
+// let path = require('path');
+// const csv = require('csv-parser');
+// let fs = require('fs');
 
+app.listen('3005');
 
 const influx = new Influx.InfluxDB({
     host: 'aworldbridgelabs.com',
@@ -15,11 +16,11 @@ const influx = new Influx.InfluxDB({
     port: 8086
 });
 
-var mysql = require('mysql');
-var http = require('http');
-var bodyParser = require("body-parser");
+const mysql = require('mysql');
+// let http = require('http');
+const bodyParser = require("body-parser");
 
-var con = mysql.createConnection({
+const con = mysql.createConnection({
     host: "10.11.90.16",
     user: "AppUser",
     password: "Special888%",
@@ -28,175 +29,53 @@ var con = mysql.createConnection({
     Table: "StationData"
 });
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+let pastTime = "4m";
+let nowTime = "2m";
+let pack=[];
+let i=0;
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
 
-function convertToCSV(objArray,m) {
-    // console.log(objArray);
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    var str = "time," + m +"\n";
-    for (var i = 0; i < array.length; i++) {
-        var line = "";
-        for (var a = 0;a<1;a++) {
-            line += array[i]["time"];
-            line += ",";
-            line += array[i]["mean"]
-        }
-        str += line + '\r\n';
-    }
-    return str;
-}
+// function convertToCSV(objArray,m) {
+//     // console.log(objArray);
+//     let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+//     let str = "time," + m +"\n";
+//     for (let i = 0; i < array.length; i++) {
+//         let line = "";
+//         for (let a = 0;a<1;a++) {
+//             line += array[i]["time"];
+//             line += ",";
+//             line += array[i]["mean"]
+//         }
+//         str += line + '\r\n';
+//     }
+//     return str;
+// }
 
-function converter(result){
-    var trares = result;
-    for(var i=0; i<result.length;i++){
-        trares[i].time=result[i].time._nanoISO;
-    }
-    return trares
-}
+// function converter(result){
+//     let trares = result;
+//     for(let i=0; i<result.length;i++){
+//         trares[i].time=result[i].time._nanoISO;
+//     }
+//     return trares
+// }
 
-function avg(res,len){
-    var arr = res.slice(Math.max(res.length - len, 1));
-    var sum = 0;
-    for(var i = 0; i<arr.length; i++){
-        sum = sum + arr[i].mean
-    }
-    var average = sum/len;
-    return average
-}
-
-var pastTime = "4m";
-var nowTime = "2m";
-
-app.get('/newWind', function (req, res) {
-    var queryHa = 'SELECT * FROM ' + req.query.stationIs + 'avg WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'";
-// console.log(queryHa);
-    influx.query(queryHa).then
-    (result => {
-        res.send(result);
-        // console.log(result)
-    }).catch(err => {
-        res.status(500).send(err.stack)
-    });
-});
-var csvData;
-function download_csv(exportFilename) {
-    if (exportFilename == null) {
-        exportFilename = timeFrom + '-' + timeTo + 'AvgESPData';
-    }
-    console.log(download);
-
-
-    csvData = new Blob([download], {type: 'text/csv;charset=utf-8;'});
-    // IE11 & Edge
-    if (navigator.msSaveBlob) {
-        navigator.msSaveBlob(csvData, exportFilename);
-    } else {
-        //In FF link must be added to DOM to be clicked
-        var link = document.createElement('a');
-        link.href = window.URL.createObjectURL(csvData);
-        link.setAttribute('download', exportFilename);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
-}
-
-app.get('/newSnow', async function (req, res) {
-    var queryH = 'SELECT * FROM ' + req.query.stationIs + ' WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'";
-    console.log(queryH);
-    var download=[];
-    await influx.query(queryH).then
-    (result => {
-        console.log("run raw data request");
-        // console.log(timeFrom+timeTo);
-
-
-
-        result.forEach(function (el, i) {
-            // var time = new Date(el.time);
-            // console.log(el.time);
-            // console.log(el.time._nanoISO);
-            // console.log(el._nanoISO);
-            var a = '' + el.X;
-            var b = '' + el.Y;
-            var c = '' + el.Z;
-            var t = el.time;
-            // download += t + ',' + a + ',' + b + ',' + c + '\n';
-            download.push({time: t, x: a, y: b, z: c});
-        });
-
-
-
-        // console.log(result)
-    }) .catch(err => {
-        // res.status(500).send(err.stack)
-    });
-
-    var exportFilename = req.query.stationName + "_" + req.query.timeFrom.slice(5,-11) +'-'+ req.query.timeTo.slice(5,-11) + 'Raw.csv';
-    // download_csv(exportFilename);
-    console.log(exportFilename);
-    console.log("finish editing result");
-    var link = 'rawData/'+exportFilename;
-    const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-    const csvWriter = createCsvWriter({
-        path: link,
-        header: [
-            {id: 'time', title: 'Time'},
-            {id: 'x', title: 'X'},
-            {id: 'y', title: 'Y'},
-            {id: 'z', title: 'Z'},
-        ]
-    });
-    console.log("data done");
-    await csvWriter
-        .writeRecords(download)
-        .then(()=> {
-            console.log('The CSV file was written successfully');
-            console.log('link is sent?');
-            console.log(link);
-            res.send(link);
-            console.log('link is sent');
-            console.log('The CSV file was written successfully~~~~~~~~~?');
-        });
-});
-
-app.get('/query', function (req, res) {
-    // console.log(req.query.stationID);
-    var query = 'SELECT * FROM ' + req.query.stationID + 'avg WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime;
-    // console.log(query);
-    influx.query(query).then
-    (result => {
-        res.send(result);
-    }).catch(err => {
-        console.log(err)
-    })
-});
-
-var pack=[];
-var i=0;
-app.get('/querys', async function (req, res) {
-    // console.log(req.query.stations);
-    // var pack = await [[req.query.stations[0],Q(req.query.stations[0])]];
-    for(i = 0; i<req.query.stations.length; i++){
-        // var result;
-        var query = 'SELECT * FROM ' + req.query.stations[i] + 'avg WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime;
-        await influx.query(query).then
-        (result => {
-            pack[i]= [req.query.stations[i],result];
-        }).catch(err => {
-            console.log(err)
-        });
-
-        if (i === req.query.stations.length - 1) {
-            res.send(pack);
-        }
-    }
-});
+// function avg(res,len){
+//     let arr = res.slice(Math.max(res.length - len, 1));
+//     let sum = 0;
+//     for(let i = 0; i<arr.length; i++){
+//         sum = sum + arr[i].mean
+//     }
+//     let average = sum/len;
+//     return average
+// }
 
 app.get ('/stations', function (req, res){
     // console.log(req);
@@ -207,4 +86,103 @@ app.get ('/stations', function (req, res){
     });
 });
 
-app.listen('3005');
+app.get('/newWind', function (req, res) {
+    let queryHa = 'SELECT * FROM ' + req.query.stationIs + 'avg WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'";
+// console.log(queryHa);
+    influx.query(queryHa).then
+    (result => {
+        res.send(result);
+        // console.log(result)
+    }).catch(err => {
+        res.status(500).send(err.stack)
+    });
+});
+// let csvData;
+// function download_csv(exportFilename) {
+//     if (exportFilename == null) {
+//         exportFilename = timeFrom + '-' + timeTo + 'AvgESPData';
+//     }
+//     console.log(download);
+//
+//
+//     csvData = new Blob([download], {type: 'text/csv;charset=utf-8;'});
+//     // IE11 & Edge
+//     if (navigator.msSaveBlob) {
+//         navigator.msSaveBlob(csvData, exportFilename);
+//     } else {
+//         //In FF link must be added to DOM to be clicked
+//         let link = document.createElement('a');
+//         link.href = window.URL.createObjectURL(csvData);
+//         link.setAttribute('download', exportFilename);
+//         document.body.appendChild(link);
+//         link.click();
+//         document.body.removeChild(link);
+//     }
+// }
+
+app.get('/newSnow', async function (req, res) {
+    let queryH = 'SELECT * FROM ' + req.query.stationIs + ' WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'";
+    // console.log(queryH);
+    let download=[];
+    await influx.query(queryH).then
+    (result => {
+        result.forEach(function (el, i) {
+            let a = '' + el.X;
+            let b = '' + el.Y;
+            let c = '' + el.Z;
+            let t = el.time;
+            // download += t + ',' + a + ',' + b + ',' + c + '\n';
+            download.push({time: t, x: a, y: b, z: c});
+        })}).catch(err => {
+        res.status(500).send(err.stack)
+    });
+
+    let exportFilename = req.query.stationName + "_" + req.query.timeFrom.slice(5,-11) +'-'+ req.query.timeTo.slice(5,-11) + 'Raw.csv';
+    let link = 'rawData/'+exportFilename;
+    const csvWriter = createCsvWriter({
+        path: link,
+        header: [
+            {id: 'time', title: 'Time'},
+            {id: 'x', title: 'X'},
+            {id: 'y', title: 'Y'},
+            {id: 'z', title: 'Z'},
+        ]
+    });
+    await csvWriter
+        .writeRecords(download)
+        .then(()=> {
+
+            res.send(link);
+        });
+});
+
+app.get('/query', function (req, res) {
+    let query = 'SELECT * FROM ' + req.query.stationID + 'avg WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime;
+    influx.query(query).then
+    (result => {
+        res.send(result);
+    }).catch(err => {
+        console.log(err)
+    })
+});
+
+// app.get('/querys', async function (req, res) {
+//     // console.log(req.query.stations);
+//     // let pack = await [[req.query.stations[0],Q(req.query.stations[0])]];
+//     for(i = 0; i<req.query.stations.length; i++){
+//         // let result;
+//         let query = 'SELECT * FROM ' + req.query.stations[i] + 'avg WHERE time >= now() - ' + pastTime + ' AND time<=now() - '+ nowTime;
+//         await influx.query(query).then
+//         (result => {
+//             pack[i]= [req.query.stations[i],result];
+//         }).catch(err => {
+//             console.log(err)
+//         });
+//
+//         if (i === req.query.stations.length - 1) {
+//             res.send(pack);
+//         }
+//     }
+// });
+
+
