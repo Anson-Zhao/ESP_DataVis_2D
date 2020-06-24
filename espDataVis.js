826a9e161104362f7336fda854c022a44064a43c
imacbig10 <lin.feng@g.northernacademy.org>
2020-06-24 16:20:04 -0400

    update dygraph libraries

--- espDataVis.js
+++ espDataVis.js
@@ -2,6 +2,7 @@ const Influx = require('influx');
 const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
+// const ib = require(ib*);
 // let jsonexport = require('jsonexport');
 // let path = require('path');
 // const csv = require('csv-parser');
@@ -36,7 +37,7 @@ const mysql = require('mysql');
 const bodyParser = require("body-parser");
 
 const con = mysql.createConnection({
-    host: "localhost",
+    host: "10.11.90.16",
     user: "AppUser",
     password: "Special888%",
     port: "3306",
@@ -95,11 +96,146 @@ app.use(bodyParser.json());
 app.get ('/stations', function (req, res){
     // console.log(req);
     con.query("SELECT StationName,City,State,StationId,Longitude,Latitude FROM ESP2.StationData Where Status = 'Active'",function (err, result) {
-        if (err) throw err;
-        // console.log(result);
+        console.log(result[0].StationId);
+        // EventCheck(result);
         res.send(result);
     });
 });
+
+function diff_minutes(dt2, dt1)
+{
+    var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= 60;
+    return Math.abs(Math.round(diff));
+}
+
+var EQstations;
+con.query("SELECT StationName,City,State,StationId,Longitude,Latitude FROM ESP2.StationData Where StationDescription = 'Earthquake'",function (err, result) {
+    // [
+    //   RowDataPacket {
+    //     StationName: 'ESP02',
+    //     City: 'Soquel',
+    //     State: 'California',
+    //     StationId: 'station_two',
+    //     Longitude: '-121.947898',
+    //     Latitude: '37.024433'
+    //   },...]
+    console.log(result[0].StationId);
+    console.log(result);
+    EQstations=result;
+    EventCheck(result);
+    // res.send(result);
+});
+var Flag=[];
+var Pair=[];
+var c=0;
+var minute="6m";
+async function EventCheck(stations){
+    console.log("all begin");
+    for(var i=0;i<stations.length;i++){
+        Flag.push([stations[i].StationName]);
+        Pair.push([stations[i].StationName]);
+        console.log(Date());
+        var querystatement='SELECT * FROM ' + stations[i].StationId + 'avg WHERE time >= now()-' +minute+ ' AND time<= now()';
+        console.log(querystatement);
+        await influx.query(querystatement).then
+        (result => {
+            // console.log(result.length);
+            // console.log(result[0].X);
+            // console.log(result[0].Y);
+            // console.log(result[0].Z);
+            // console.log(result[0].time._nanoISO);
+            for (var a = 0; a < result.length; a++) {
+                // DifA = result[a + 1].X - result[a].X;
+                DifB = result[a + 2].X - result[a + 1].X;
+                // console.log(DifA,DifB);
+                if (Math.abs(DifB) > 9) {
+                    Flag[i].push({
+                        stationInfo: stations[i],
+                        time: result[a].time._nanoISO,
+                        X: result[a + 2].X,
+                        Y: result[a + 2].Y,
+                        Z: result[a + 2].Z,
+                        Diff: DifB
+                    });
+                    // DifA = null;
+                    DifB = null;
+                } else {
+                    // DifA = null;
+                    DifB = null;
+                }
+                if (a === result.length - 3) {
+                    console.log("flag done at"+Date());
+                    pair()
+                }
+            }
+
+            function pair() {
+                console.log("Pair begin at"+Date());
+                for (var b = 1; b < Flag[i].length; b++) {
+                    // Flag[b].stationInfo===Flag[b+1].stationInfo&&
+
+                    if (Date.parse(Flag[b + 1].time) - Date.parse(Flag[b].time) < 10800000 && Flag[b].Diff * Flag[b + 1].Diff < 0) {
+                        Pair[i].push([Flag[i][b], Flag[i][b + 1]]);
+                        Flag[i].splice(b,2)
+                        //so the format will look like [ [{},{}] ]
+                        //when finish compair, it will be deleted.
+                    } else {
+                        Flag[i].splice(b,1);
+                        b++;
+                    }
+                    if (b === Flag[i].length - 1) {
+                        console.log("pair done at"+Date())
+                    }
+                }
+            }
+        }).catch(err => {
+        });
+        console.log("Flag length:"+Flag[i].length);
+        console.log(Pair);
+        console.log("Pair Length:"+Pair[i].length);
+        console.log(Date()+"one station end");
+    }
+    console.log("All end");
+    // console.log(Flag);
+
+    console.log(Date());
+    setInterval(function () {
+        EventCheck(EQstations)}, 300000);
+}
+
+// testgroup=[];
+// test1();
+// function test1(result){
+//     for(var a=0;a<result.length;a++) {
+//         // DifA = result[a + 1].X - result[a].X;
+//         DifB = result[a + 2].X - result[a + 1].X;
+//         if (Math.abs(DifB) > 9) {
+//             Flag.push({stationInfo:stations[i],time:result[a].time._nanoISO, X:result[a + 2].X, Y:result[a + 2].Y, Z:result[a + 2].Z, Diff:DifB});
+//             DifA = null;
+//             DifB = null;
+//         } else {
+//             DifA = null;
+//             DifB = null;
+//         }
+//     }
+//     for(var b=0; a<Flag.length;b++) {
+//     if (Flag[b].stationInfo===Flag[b+1].stationInfo&&Date.parse(Flag[b + 1].time) - Date.parse(Flag[b].time) < 10800000&&Flag[b].Diff * Flag[b + 1].Diff < 0) {
+//         Pair.push([Flag[b],Flag[b+1]])
+//         //so the format will look like [ [{},{}] ]
+//     } else {
+//         b++;
+//     }
+// }
+// }
+// console.log(Date.parse("2020-05-31T16:42:35.000Z"));
+// console.log(Date.parse("2020-05-31T16:42:35.000Z") - Date.parse("2020-05-31T16:42:37.000Z"));
+// if (Date.parse(EndT) - Date.parse(BeginT) > 10800000) {
+//     BeginT = EndT = null;
+// }
+
+
+
 
 app.get('/newWind', function (req, res) {
     let queryHa = 'SELECT * FROM ' + req.query.stationIs + 'avg WHERE time >= ' + "'" + req.query.timeFrom + "'" + ' AND time<= ' + "'" + req.query.timeTo + "'";
@@ -210,6 +346,8 @@ app.get('/mail', function (req, res){
 //         }
 //     }
 // });
+
+
 
 
 
